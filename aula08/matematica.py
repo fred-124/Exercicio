2cee5efa12663c7c07c1ac937ce1444bf6b1a057
fred-124 <fredericocunha131314@gmail.com>
2025-10-03 21:38:29 -0300

    Create matematica.py

---
+++ aula08/matematica.py
@@ -0,0 +1,24 @@
+import math
+
+def soma(a, b):
+    return a + b
+
+def subtracao(a, b):
+    return a - b
+
+def multiplicacao(a, b):
+    return a * b
+
+def divisao(a, b):
+    return a / b
+
+def porcentagem(a, b):
+    return a * b / 100
+
+def potencia(a, b):
+    return a ** b
+
+def raiz(a):
+    return math.sqrt(a)
+
+
